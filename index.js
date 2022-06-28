4bf7dbffd7c89dbf42faa950a635ae917ac21855
Quanysh <anuarkhanov@gmail.com>
2022-06-28 20:18:27 +0600

    ddd

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.getElementById("toggle");
+  const bg = document.querySelectorAll(".bg-light");
+  const text = document.querySelectorAll(".text-dark");
 
-  toggleBtn.addEventListener("input", () => {
-    const bg = document.querySelectorAll('.bg-light')
-    const text = document.querySelectorAll('.text-dark')
-    bg.forEach(el => {
-        el.classList.toggle('bg-dark')
-    })
-    text.forEach(el => {
-        el.classList.toggle('text-white')
-    })
+  const isDark = Boolean(Number(localStorage.getItem("isDark")));
+  toggleBtn.checked = isDark;
+
+  if (isDark) {
+    bg.forEach((el) => {
+      el.classList.toggle("bg-dark");
+    });
+    text.forEach((el) => {
+      el.classList.toggle("text-white");
+    });
+  }
+
+  toggleBtn.addEventListener("input", (e) => {
+    const isDark = e.target.checked;
+    localStorage.setItem("isDark", isDark ? "1" : "0");
+    bg.forEach((el) => {
+      el.classList.toggle("bg-dark");
+    });
+    text.forEach((el) => {
+      el.classList.toggle("text-white");
+    });
   });
 });
